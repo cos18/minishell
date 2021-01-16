22333182ec2a97e3e8d069488bfc2b04d2785611
Sungwoo Park <codest99@gmail.com>
2021-01-16 15:53:23 +0900

    fix only dollar sign segfault

--- srcs/env/env_util.c
+++ srcs/env/env_util.c
@@ -6,7 +6,7 @@
 /*   By: sunpark <sunpark@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/13 14:24:15 by sunpark           #+#    #+#             */
-/*   Updated: 2021/01/16 15:13:10 by sunpark          ###   ########.fr       */
+/*   Updated: 2021/01/16 15:52:31 by sunpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,22 +37,23 @@ int				env_valid_name(char *name, int is_export)
 
 t_envlst		*get_env_in_token(char **check)
 {
-	t_envlst	*result;
 	char		*start;
+	t_envlst	*result;
 	char		*name;
 
-	start = ++(*check);
 	if (**check == '?')
 		return (g_bash->exit_status);
+	start = ++(*check);
+	if (!ft_isalnum(**check) && **check != '_')
+	{
+		(*check)--;
+		return (g_envlst_first_wrong);
+	}
+	if (ft_isnumber(**check))
+		return (NULL);
 	while (**check)
 	{
-		if (start == *check && !ft_isalnum(**check) && **check != '_')
-		{
-			(*check)--;
-			return (g_envlst_first_wrong);
-		}
-		if ((start == *check && ft_isnumber(**check)) ||
-				(!ft_isalnum(*(*check + 1)) && *(*check + 1) != '_'))
+		if (!ft_isalnum(*(*check + 1)) && *(*check + 1) != '_')
 			break ;
 		(*check)++;
 	}
